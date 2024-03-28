1adf23c7e92d7b970c948b62085106d72a835b03
Vasiliy Yankovsky <yankvasya@gmail.com>
2024-03-28 22:27:09 +0400

    tmp

---
+++ src/components/UI/CButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    type="button"
+    class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    :class="{
+      'pointer-events-none': disabled,
+    }"
+    :disabled="disabled"
+    @click="buttonClick"
+  >
+    <slot name="before-text" />
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      disabled: false,
+    },
+  },
+  emits: {
+    buttonClick() {
+      return true;
+    },
+  },
+  methods: {
+    buttonClick() {
+      this.$emit("buttonClick");
+    },
+  },
+};
+</script>
